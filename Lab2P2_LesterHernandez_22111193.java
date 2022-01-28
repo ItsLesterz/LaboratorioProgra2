25a339102523b9549b93789c9fdbee341cc9241b
leste <lesterherdez03@unitec.edu>
2022-01-28 16:07:37 -0600

    9no avance(me salte del 2do al 4to sorry :c)

--- Lab2P2_LesterHernandez_22111193.java
+++ Lab2P2_LesterHernandez_22111193.java
@@ -1,8 +1,39 @@
 package LabProgra2;
 import javax.xml.bind.SchemaOutputResolver;
 
+import static LabProgra2.Animales.*;
 
 public class Lab2P2_LesterHernandez_22111193 {
+    public static void main(String[] args) {
+        System.out.println("-------Registro de Animales (Discovery)-------");
+        System.out.print("Seas bienvenido al mejor registro de animales (Discovery)" +
+                "\n(1). Registrar Animal" +
+                "\n(2). Editar Animal" +
+                "\n(3). Imprimir Listado de Animales" +
+                "\n(4). Alimentar Animales" +
+                "\n(5). Salir");
+        System.out.print("SELECCIONE OPCION: ");
+        int opcion=leer.nextInt();
+        do{
+            switch (opcion){
+                case 1:
 
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+            }
+
+        }while(opcion!=5)
+    }
 
 }
