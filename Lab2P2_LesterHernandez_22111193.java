dacef9b33d66f34fa1e41a8ebac2b430767abbe4
leste <lesterherdez03@unitec.edu>
2022-01-28 16:19:30 -0600

    11mo avance

--- Lab2P2_LesterHernandez_22111193.java
+++ Lab2P2_LesterHernandez_22111193.java
@@ -17,21 +17,27 @@ public class Lab2P2_LesterHernandez_22111193 {
         do{
             switch (opcion){
                 case 1:
-
+                    System.out.println("Ingrese el Nombre Cientifico: ");
+                    String nombre=leer.next();
+                    verify(nombre);
+                    registrarAnimal(nombre);
                     break;
                 case 2:
-
+                    System.out.println("Ingrese el nombre cientifico del animal: ");
+                    String nombre2=leer.next();
+                    editAnimalCientifico(nombre2);
                     break;
                 case 3:
-
+                    imprimirAnimales();
                     break;
                 case 4:
-
+                    cadenaAlimenticia();
                     break;
                 case 5:
-
+                    System.out.println("Ha salido del registro de animales(Discovery)!");
                     break;
                 default:
+                    System.out.println("Opcion invalida!!!!");
                     break;
             }
 
